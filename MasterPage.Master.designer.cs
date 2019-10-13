f8f51120b3c627ef6b3cb2a212234db99767ff2c
Dean Shannon <dean.shannon@cqumail.com>
2019-10-13 15:17:55 +1000

    add login name to master from default

--- MasterPage.Master.designer.cs
+++ MasterPage.Master.designer.cs
@@ -7,11 +7,13 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace KFurniture {
-    
-    
-    public partial class Site1 {
-        
+namespace KFurniture
+{
+
+
+    public partial class Site1
+    {
+
         /// <summary>
         /// head control.
         /// </summary>
@@ -20,7 +22,7 @@ namespace KFurniture {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
-        
+
         /// <summary>
         /// form1 control.
         /// </summary>
@@ -29,7 +31,7 @@ namespace KFurniture {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
+
         /// <summary>
         /// SiteMapDataSource2 control.
         /// </summary>
@@ -38,7 +40,7 @@ namespace KFurniture {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.SiteMapDataSource SiteMapDataSource2;
-        
+
         /// <summary>
         /// SiteMapPath1 control.
         /// </summary>
@@ -47,7 +49,25 @@ namespace KFurniture {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.SiteMapPath SiteMapPath1;
-        
+
+        /// <summary>
+        /// LoginName1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LoginName LoginName1;
+
+        /// <summary>
+        /// LoginStatus1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LoginStatus LoginStatus1;
+
         /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
